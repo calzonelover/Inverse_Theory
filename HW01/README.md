6062f88afaa6caf752e620e43c6b1c681e499abc
Patomp <patomporn.pay@gmail.com>
2019-09-04 16:03:40 +0700

    not finish pb3

--- HW01/README.md
+++ HW01/README.md
@@ -20,3 +20,24 @@ Parameter a = 1.0015782107490603, b = -0.4968111847386467
 ```
 
 ## Problem 3
+
+### Pesudocode
+
+* Notation for the relations
+    * <a href="https://www.codecogs.com/eqnedit.php?latex=R_{ki}&space;\leftarrow&space;[I(d-Am_{ki}))]^{-1}" target="_blank"><img src="https://latex.codecogs.com/svg.latex?R_{ki}&space;\leftarrow&space;[I(d-Am_{ki}))]^{-1}" title="R_{ki} \leftarrow [I(d-Am_{ki}))]^{-1}" /></a>
+    * <a href="https://www.codecogs.com/eqnedit.php?latex=m_{kf}&space;\leftarrow&space;(A^TR_{ki}A)^{-1}A^TR_{ki}d" target="_blank"><img src="https://latex.codecogs.com/svg.latex?m_{kf}&space;\leftarrow&space;(A^TR_{ki}A)^{-1}A^TR_{ki}d" title="m_{kf} \leftarrow (A^TR_{ki}A)^{-1}A^TR_{ki}d" /></a>
+    * <a href="https://www.codecogs.com/eqnedit.php?latex=\epsilon_{\text{now}}&space;\leftarrow&space;\frac{||&space;m_{kf}&space;-&space;m_{ki}&space;||}{1&plus;||m_{kf}||}" target="_blank"><img src="https://latex.codecogs.com/svg.latex?\epsilon_{\text{now}}&space;\leftarrow&space;\frac{||&space;m_{kf}&space;-&space;m_{ki}&space;||}{1&plus;||m_{kf}||}" title="\epsilon_{\text{now}} \leftarrow \frac{|| m_{kf} - m_{ki} ||}{1+||m_{kf}||}" /></a>
+
+```
+func get_model_by_l1r(A, d, m_0, epsilon)
+    m_ki = m_0
+    while true do
+        R_ki <- get_R(A, d, m_ki)
+        m_kf <- get_m(A, d, R_ki)
+        e_now <- get_e(m_ki, m_kf)
+        if e_now < epsilon then
+            break
+        m_ki <- m_kf
+    return m_kf
+end func
+```
--- HW01/pb3.py
+++ HW01/pb3.py
@@ -5,6 +5,7 @@ import random
 N = 20
 N_OUTLIER = 2
 DISTANCE_BIAS = 3
+EPSILON = 1e-6 # tolerance
 
 a = 1.0
 b = -0.5
@@ -27,9 +28,34 @@ def get_sampling(n, n_outlier, distance_bias=DISTANCE_BIAS, max_x=MAX_X, min_x=M
         y_samples[index_outlier] += random.choice([distance_bias, -distance_bias])
     return {'x': x_samples.reshape(-1,1), 'y': y_samples.reshape(-1,1)}
 
+# SLQ
 def get_model_by_slq(A, d):
 	inverse_A = np.linalg.inv(np.matmul(A.T, A))
 	return np.matmul(inverse_A.T, np.matmul(A.T, d))
+
+# L1
+def get_R(A, d, m_ki):
+	r = np.subtract(d, np.matmul(A, m_ki))
+	return np.linalg.inv(np.multiply(np.identity(N, dtype=float), r))
+
+def get_m(A, d, R_ki):
+	inv_term = np.linalg.inv(np.matmul(A.T, np.matmul(R_ki, A)))
+	return np.matmul(inv_term, np.matmul(A.T, np.matmul(R_ki, d)))
+
+def get_e(m_ki, m_kf):
+	return np.linalg.norm(np.subtract(m_kf, m_ki))/(1+np.linalg.norm(m_kf))
+
+def get_model_by_l1r(A, d, m_0, epsilon):
+	m_ki = m_0
+	while True:
+		R_ki = get_R(A, d, m_ki)
+		m_kf = get_m(A, d, R_ki)
+		e_now = get_e(m_ki, m_kf)
+		if e_now < epsilon:
+			break
+		m_ki = m_kf
+		# print(e_now, m_kf)
+	return m_kf
 
 if __name__ == "__main__":
 	samples = get_sampling(N, N_OUTLIER)
@@ -40,6 +66,10 @@ if __name__ == "__main__":
 	print("Standard least squares method")
 	m_slq = get_model_by_slq(A, d)
 	print("Parameter a = {}, b = {}".format(m_slq[0, 0], m_slq[1, 0]))
+
+	print("L1 Regression method")
+	m_l1r = get_model_by_l1r(A, d, m_slq, EPSILON)
+	print("Parameter a = {}, b = {}".format(m_l1r[0, 0], m_l1r[1, 0]))
 
     # visual
 	plt.plot(samples['x'], samples['y'], 'ro', label="y = ax + b + $\mathcal{N}$(0, 0.2)")
