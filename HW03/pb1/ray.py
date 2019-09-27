29bc5b421e1e4d03348177a664f3c3e826f1268a
physics <patomporn.pay@gmail.com>
2019-09-28 02:35:07 +0700

    backup

--- HW03/pb1/ray.py
+++ HW03/pb1/ray.py
@@ -1,4 +1,5 @@
-
+# Reference
+# https://www.doc.ic.ac.uk/~dfg/graphics/graphics2008/GraphicsLecture09.pdf
 
 def ray(nx, ny, dx, x1, z1, x2, z2):
     # case 1 same
@@ -13,7 +14,7 @@ def ray(nx, ny, dx, x1, z1, x2, z2):
     # case 4 tilt
     else:
         m = (z2-z1)/(x2-x1)
-        c = -m*x1 + z1
+        c = z1 - m*x1
         
 
 # def path(s_i, r_i, R):
--- Lec06/saleman.py
+++ Lec06/saleman.py
@@ -5,10 +5,12 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-HOUSE = 6
-T = 30000
+HOUSE = 20
+T = 300
 T_DECAY = 0.9
 N_ACCEPTED_MAX = 10
+
+KEEP_DISTANCE = []
 
 REGION = np.array([
     [np.random.uniform(100,-100), np.random.uniform(100,-100)] for i in range(HOUSE)
@@ -17,16 +19,16 @@ REGION = np.array([
 def compute_path(sequence):
     distance = 0
     for i in range(len(sequence)-1):
-        distance += np.sqrt(np.sum(np.square(np.subtract(REGION[sequence[i+1]], REGION[sequence[i]]))))
+        distance += np.sqrt(np.sum(np.square(np.subtract(
+            REGION[sequence[i+1]], REGION[sequence[i]]
+        ))))
     return distance
 
 def swop_sequence(original_sequence, n_region):
-    sequence = original_sequence
+    sequence = list(map(lambda x: x, original_sequence))
     i = np.random.randint(n_region)
     j = np.random.randint(n_region)
     sequence[i], sequence[j] = sequence[j], sequence[i]
-    print(i, j)
-    print(sequence, original_sequence)
     return sequence
 
 if __name__ == "__main__":
@@ -36,8 +38,7 @@ if __name__ == "__main__":
     t = T
     n = 0
     distance_old = 100
-    while t > 1.0:
-        # print("! loop !")
+    while t > 1e-3:
         while n < N_ACCEPTED_MAX:
             # x <- x + dx
             sequence = swop_sequence(accepted_sequence, n_region)
@@ -49,22 +50,15 @@ if __name__ == "__main__":
                 accepted_sequence = sequence
             else:
                 prob = math.e**(-diff_distance/t)
-                # print(diff_distance)
                 if np.random.uniform(0,1) < prob:
                     n += 1
                     accepted_sequence = sequence
-        # print(accepted_sequence)
-        # print(compute_path(accepted_sequence))
-        # print("! end loop !")
+        KEEP_DISTANCE.append(compute_path(accepted_sequence))
         n = 0
         t *= T_DECAY
-        # print("!", distance)
     print("End t = {}".format(t))
     # visulize
     plt.plot(REGION[:, 0], REGION[:, 1], 'o')
-    
-    # print(accepted_sequence)
-    # print(compute_path(accepted_sequence))
 
     path_line = np.array([REGION[i] for i in accepted_sequence])
     plt.plot(path_line[:, 0], path_line[:, 1], '.-')
@@ -73,4 +67,17 @@ if __name__ == "__main__":
     plt.plot(REGION[accepted_sequence[-1], 0], REGION[accepted_sequence[-1], 1], 'go', label="end")
     plt.legend()
 
-    plt.show()
+    plt.title('Saleman walking path')
+    plt.xlabel('x')
+    plt.ylabel('y')
+
+    plt.savefig('walking_path.png')
+    # plt.show()
+
+    plt.clf()
+    plt.plot(KEEP_DISTANCE)
+    plt.title('Distance over iteration')
+    plt.xlabel('k')
+    plt.ylabel('Distance')
+    plt.savefig('distance.png')
+    # plt.show()
