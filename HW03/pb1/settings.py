ed23847bc99f17f913f60e9053a6f5775ff3293d
Patomp <patomporn.pay@gmail.com>
2019-09-29 03:16:01 +0700

    backup

--- HW03/pb1/settings.py
+++ HW03/pb1/settings.py
@@ -1,3 +1,11 @@
+import numpy as np
+
 FILENAME = "src/vel_nx50_nz50_dx20.dat"
 NX = NY = 50
 DX = 20
+
+N_SOURCE = 50
+N_RECEIVER = 50
+
+# ALPHAS = [1e-3, ]
+ALPHAS = np.logspace(-4, 4, num=20, base=10)
--- HW03/pb1/stt.py
+++ HW03/pb1/stt.py
@@ -7,26 +7,74 @@ import matplotlib.pyplot as plt
 
 import settings, ray
 
-def readmap(filename):
+REPORT_LOG = {
+    'alphas': [],
+    'norm_model': [],
+    'norm_res': []
+}
+
+def readraw(filename):
     f = open(settings.FILENAME, "r")
-    a = np.fromfile(f, dtype=np.float32)
+    return np.fromfile(f, dtype=np.float32)
+
+def readmap(filename):
+    a = readraw(filename)
     a = a.reshape(settings.NX, settings.NY)
     return a.T
 
 if __name__ == "__main__":
-    raw_map = readmap(filename=settings.FILENAME)
-    for i_y in range(40):
-        y = 40 + settings.DX * i_y
-        one_ray_map = ray.ray_length(-10,210,1010,y+10).reshape(50, 50)
-        plt.imshow(one_ray_map, extent=[0, settings.DX*settings.NX, settings.DX*settings.NY, 0])
-        plt.colorbar()
+    s_real = readraw(filename=settings.FILENAME)
+
+    dy_sr = (settings.NY * settings.DX)/settings.N_SOURCE
+    for alpha in settings.ALPHAS:
+        print(alpha)
+        t = []
+        l = []
+        for s_i in range(settings.N_SOURCE):
+            for r_j in range(settings.N_RECEIVER):
+                y_s_i = settings.DX/2 + s_i * dy_sr
+                y_r_i = settings.DX/2 + r_j * dy_sr
+
+                l_i = ray.ray_length(-10,y_s_i,1010,y_r_i)
+                l.append(l_i)
+                t.append(np.matmul(s_real.T, l_i))
+        t = np.array(t)
+        l = np.array(l)
+        s_model = np.matmul(
+            np.linalg.inv(
+                np.add(
+                    np.matmul(l.T, l),
+                    np.multiply(alpha, np.identity(settings.NX*settings.NY, dtype=float))
+                )
+            ),
+            np.matmul(l.T, t)
+        )
+        REPORT_LOG['alphas'] = alpha
+        REPORT_LOG['norm_model'] = np.linalg.norm(s_model)
+        REPORT_LOG['norm_res'] = np.linalg.norm(
+            np.subtract(
+                t,
+                np.multiply(s_model.T, l)
+        ))
+        # visualize model
+        map_model = s_model.reshape(settings.NX, settings.NY).T
+        plt.imshow(map_model, cmap='jet', extent=[0, settings.DX*settings.NX, settings.DX*settings.NY, 0])
+        a = plt.colorbar()
+        a.set_label('$v$')
+        plt.title("Model Velocity (alpha = {:6.6f})".format(alpha))
         plt.xlabel("$x$")
         plt.ylabel("$y$")
-        plt.savefig("img/ray_{:02.0f}.png".format(i_y))
+        plt.savefig("img/model_v_alpha{}.png".format(alpha))
         plt.clf()
         # plt.show()
 
-    # visualize
+    # visual l-curve
+    plt.plot(REPORT_LOG['norm_res'], REPORT_LOG['norm_model'], 'o-')
+    plt.savefig('L_curve.png')
+    plt.clf()
+
+    # visualize raw
+    raw_map = readmap(filename=settings.FILENAME)
     plt.imshow(raw_map, cmap='jet', extent=[0, settings.DX*settings.NX, settings.DX*settings.NY, 0])
     a = plt.colorbar()
     a.set_label('$v$')
@@ -34,4 +82,19 @@ if __name__ == "__main__":
     plt.xlabel("$x$")
     plt.ylabel("$y$")
     plt.savefig("real_v.png")
-    # plt.show()
+    plt.show()
+
+    '''
+    # Test ray tracing
+    for i_y in range(40):
+        y = 10 + settings.DX * i_y
+        one_ray_map = ray.ray_length(-10,510,1010,y).reshape(50, 50)
+        plt.imshow(one_ray_map, extent=[0, settings.DX*settings.NX, settings.DX*settings.NY, 0])
+        a = plt.colorbar()
+        a.set_label('Ray length (m)')
+        plt.xlabel("$x$")
+        plt.ylabel("$y$")
+        plt.title("Ray Tracing (source: ({},{}), receiver({},{}))".format(-10, 510, 1010, y))
+        plt.savefig("img/ray_{:02.0f}.png".format(i_y))
+        plt.clf()
+    '''
