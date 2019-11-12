303a860ad85e1e92853e2c017fe9abe43e2b38b3
patomp <patomporn.pay@gmail.com>
2019-11-12 18:36:46 +0700

    writing server executable

--- HW05/pb3/regularization.py
+++ HW05/pb3/regularization.py
@@ -9,7 +9,13 @@ from scipy.ndimage import gaussian_filter, uniform_filter
 
 import utility, settings
 
+K_STOP = 10
 ALPHAS = np.logspace(-3.0, 3.0, num=20, base=10)
+REPORT_LOG = {
+    'alphas': [],
+    'norm_model': [],
+    'norm_res': [],
+}
 
 def main():
     v_real = utility.readraw(filename=settings.FILENAME)
@@ -17,12 +23,12 @@ def main():
     L_real = utility.get_l(s_real, recalculate=True)
     t_obs = np.matmul(L_real, s_real)
 
-    s_model = utility.smooth_map(s_real, mode='uniform')
-    L = utility.get_l(s_model, recalculate=True)
+    s_model0 = utility.smooth_map(s_real, mode='uniform')
+    L0 = utility.get_l(s_model0, recalculate=True)
 
-    v_model = np.divide(1.0, s_model)
+    v_model0 = np.divide(1.0, s_model0)
     plt.imshow(
-        v_model.reshape(settings.NY, settings.NX), cmap='jet',
+        v_model0.reshape(settings.NY, settings.NX), cmap='jet',
         extent=[0, settings.DX*settings.NX, settings.DX*settings.NY, 0],
         vmin=settings.COLOR_VMIN, vmax=settings.COLOR_VMAX,
     )
@@ -31,23 +37,65 @@ def main():
     plt.title("Initial Model Velocity")
     plt.xlabel("$x$")
     plt.ylabel("$y$")
-    plt.show()
+    plt.savefig(os.path.join('pb3', 'model_v0.png'))
     plt.clf()
 
-    for alpha in ALPHAS:
+    for i_alpha, alpha in enumerate(ALPHAS):
         print(alpha)
-        r = utility.get_r(t_obs, s_model, L, magnitude=False)
-        s_model = np.matmul(
-            np.linalg.inv(
-                np.add(
-                    np.matmul(L.T, L),
-                    np.multiply(alpha, np.identity(settings.NX*settings.NY, dtype=float))
-                )
-            ),
-            np.matmul(L.T, r)
-        )
-        L = utility.get_l(s_model, recalculate=True)
+        s_model = s_model0
+        L = L0
+        LOG_RES = []
+        dir_save = os.path.join('pb3', 'alpha%d'%i_alpha)
+        if not os.path.exists(dir_save):
+            os.makedirs(dir_save)
+        ## for k
+        for k in range(K_STOP):
+            r = utility.get_r(t_obs, s_model, L, magnitude=False)
+            pk = np.negative(np.matmul(
+                np.linalg.inv(
+                    np.add(
+                        np.matmul(L.T, L),
+                        np.multiply(alpha, np.identity(settings.NX*settings.NY, dtype=float))
+                    )
+                ),
+                np.matmul(L.T, r)
+            ))
+            # pk = np.divide(pk, np.max(np.abs(pk)))
+            pk = utility.smooth_map(pk, mode='uniform', kernel_size=(10, 40))
 
+            s_model = np.add(
+                s_model,
+                pk
+            )
+            L = utility.get_l(s_model, recalculate=True)
+            res = utility.get_r(t_obs, s_model, L)
+            LOG_RES.append(res)
+
+            plt.imshow(
+                pk.reshape(settings.NY, settings.NX), cmap='jet',
+                extent=[0, settings.DX*settings.NX, settings.DX*settings.NY, 0],
+            )
+            a = plt.colorbar()
+            a.set_label('$p_k$')
+            plt.title("$p_k$ (LM, alpha={0:03f}, k={1})".format(alpha, k))
+            plt.xlabel("$x$")
+            plt.ylabel("$y$")
+            plt.savefig(os.path.join(dir_save, 'pk_k%d.png'%k))
+            plt.clf()
+
+            print(alpha, k, res, np.min(s_model), np.max(s_model))
+        ## end for k
+        REPORT_LOG['alphas'].append(alpha)
+        REPORT_LOG['norm_model'].append(np.linalg.norm(s_model))
+        REPORT_LOG['norm_res'].append(res)
+        ## visual res
+        plt.plot(LOG_RES)
+        plt.xlabel('k')
+        plt.ylabel('Residual')
+        plt.title("Residual over iterations")
+        plt.savefig(os.path.join(dir_save, 'res.png'))
+        plt.clf()
+        ## visual model 
         v_model = np.divide(1.0, s_model)
         plt.imshow(
             v_model.reshape(settings.NY, settings.NX), cmap='jet',
@@ -56,8 +104,20 @@ def main():
         )
         a = plt.colorbar()
         a.set_label('$v$')
-        plt.title("Model Velocity (REG, alpha={})".format(alpha))
+        plt.title("Model Velocity (LM, alpha={0:03f}, k={1})".format(alpha, K_STOP))
         plt.xlabel("$x$")
         plt.ylabel("$y$")
-        plt.savefig(os.path.join('pb3'), 'model_v_alpha{0:.03d}.png'.format(alpha))
+        plt.savefig(os.path.join(dir_save, 'model_v_alpha{}.png'.format(i_alpha)))
         plt.clf()
+    # log l-curve
+    f_curve = open(os.path.join("pb3","l_curve.log"), 'w')
+    f_curve.write("alpha,norm_res,norm_model\n")
+    for i in range(len(REPORT_LOG['norm_res'])):
+        f_curve.write("{},{},{}\n".format(
+            REPORT_LOG['alphas'],
+            REPORT_LOG['norm_res'][i],
+            REPORT_LOG['norm_model'][i])
+        )
+    f_curve.close()
+    
+    print('The process in LM is fully finish')
--- HW05/program.sh
+++ HW05/program.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH -J InvSD # Job name 
+#SBATCH -J InvLM # Job name 
 #SBATCH --partition long
-#SBATCH -o log_InvSD.out # Name of stdout output file (%j becomes %jobId)
+#SBATCH -o log_InvLM.out # Name of stdout output file (%j becomes %jobId)
 #SBATCH -N 1 # Total number of nodes requested
-#SBATCH -t 72:00:00 # Run time (hh:mm:ss) - 1.0 hours
+#SBATCH -t 70:00:00 # Run time (hh:mm:ss) - 1.0 hours
 
 module load anaconda/3.7
 
-python main.py
+python -W ignore main.py
 
